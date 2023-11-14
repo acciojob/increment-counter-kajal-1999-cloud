1a79b49e1ab6041887c2d55210032f7af7ae8af0
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-11-14 23:05:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+function incrementCounter() {
+      // Get the current counter value
+      const counterElement = document.getElementById('counter');
+      const currentCounterValue = parseInt(counterElement.textContent, 10);
+
+      // Display the un-incremented value in an alert
+      alert( currentCounterValue);
+
+      // Increment the counter value and update the display
+      counterElement.textContent = currentCounterValue + 1;
+    }//your JS code here. If required.
+//your JS code here. If required.
+function incrementCounter() {
+      // Get the current counter value
+      const counterElement = document.getElementById('counter');
+      const currentCounterValue = parseInt(counterElement.textContent, 10);
+
+      // Display the un-incremented value in an alert
+      alert( currentCounterValue);
+
+      // Increment the counter value and update the display
+      counterElement.textContent = currentCounterValue + 1;
+    }
